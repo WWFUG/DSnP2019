cf6c228e8e1b1aa3f1c744d35eca1df58d1c4f97
WWFUG <bmpss95156@gmail.com>
2019-08-17 00:16:11 +0800

    HW1 finished

---
+++ HW1/p2/p2Json.h
@@ -0,0 +1,42 @@
+#ifndef P2JSON_H
+#define P2JSON_H
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+class Json;
+class JsonElem;
+
+class Json
+{
+private:
+    vector<JsonElem> _obj;
+    bool line2Elem(const string&);
+
+public:
+    Json(){};
+    bool isEmpty() const;
+    bool read(const string&);
+    bool add(const string&, int);
+    int sum() const;
+    const JsonElem& max() const;
+    const JsonElem& min() const;
+    int ave() const;
+    void print() const;
+
+};
+
+class JsonElem
+{
+private:
+    string _key;
+    int _value;
+public:
+    JsonElem ();
+    JsonElem(const string& k, int v): _key(k), _value(v) {};
+    string getKey() const {return _key;};
+    int getVal() const {return _value;};
+    friend ostream& operator << (ostream&, const JsonElem&);
+    
+};
+#endif /* P2JSON_H */
---
+++ HW1/p2/p2Main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+#include "p2Json.h"
+using namespace std;
+
+
+int main(){
+    Json json;
+    string fname;
+    string command;
+    cout << "Please enter the file name: ";
+    cin >> fname;
+    if(json.read(fname)){
+        cout << "File \"" << fname << "\" " << "was read in successfully" << endl;
+    }
+    else{
+        return 0;
+    }
+    do{
+        cout << "Enter command: ";
+        cin >> command;
+        if (command=="PRINT") json.print();
+        else if(command=="SUM"){
+            cout << "The summation of the values is: " << json.sum() << "." << endl;
+        }
+        else if(command=="AVE"){
+            cout << "The average of the values is: " << fixed << setprecision(1) << json.ave() << "." << endl;
+        }
+        else if(command=="MAX"){
+            cout << "The maximum element is: { " << json.max() << " }." << endl;
+        }
+        else if(command=="MIN"){
+            cout << "The minimum element is: { " << json.min() << " }." << endl;
+        }
+        else if(command=="ADD"){
+            string key,value;
+            cin >> key >> value;
+            json.add(key, stoi(value));
+        }
+        else if(command=="EXIT") break;
+        else cout << "Not a command. Please try again." << endl;
+    }while(true); 
+}
